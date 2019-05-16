c92683af1cb639c676fb38ad66bf6f9859452c92
liam <liam>
2019-05-16 17:06:17 +0200

    start

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='freesound',
+      version='0.1',
+      description='',
+      author='Liam Schoneveld',
+      author_email='liam.sch@gmail.com',
+      packages=['freesound'])
